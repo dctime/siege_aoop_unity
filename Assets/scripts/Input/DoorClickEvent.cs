03a420bb4a7f0cd21383ee4e3444c6c8b03331f1
dctime <daniel.chu916@gmail.com>
2024-01-20 10:44:39 +0800

    Feature: Door Object Animation Done

--- Assets/scripts/Input/DoorClickEvent.cs
+++ Assets/scripts/Input/DoorClickEvent.cs
@@ -6,30 +6,32 @@ using UnityEngine.EventSystems;
 public class DoorClickEvent : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerClickHandler, IPointerExitHandler, IPointerEnterHandler
 {
     [SerializeField]
-    private GameObject door;
+    private DoorOpenAnimatorController controller;
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        door.GetComponent<ExtendDirection>().SwapDirection();
+
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        Debug.Log("Window Down");
+        Debug.Log("Open the door");
+        controller.PlayOpenNegativeAnimation();
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        Debug.Log("Window Enter");
+
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        Debug.Log("Window Exit");
+
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
-        Debug.Log("Window Up");
+        Debug.Log("Close the Door");
+        controller.PlayCloseAnimation();
     }
 }
